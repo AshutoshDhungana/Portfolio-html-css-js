81a69bd93ee94d787edffc4ef74725a1ca42a64c
LuckyXBlader <63156807+AshutoshDhungana@users.noreply.github.com>
2024-04-08 23:43:21 +0545

    change-04/08

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemenu() {
+    const menu = document.querySelector(".menu_links");
+    const icon = document.querySelector(".hamburger_icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+
+
+
+}
